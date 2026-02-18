4fa85343fbeef516dafbda78e9074ad5127bc2fd
Agent Pix <agentpix@protonmail.com>
2026-02-18 13:13:51 +0100

    Integrated Firebase configuration using AngularFire

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBwNnCFj6GuRnvLz5GzLTi_sKaw2MeODQc",
+    authDomain: "igniteqs.firebaseapp.com",
+    projectId: "igniteqs",
+    storageBucket: "igniteqs.firebasestorage.app",
+    messagingSenderId: "562040819570",
+    appId: "1:562040819570:web:aaf09a1f8cd917020117eb",
+    measurementId: "G-V605FH85TN",
+  },
+};
